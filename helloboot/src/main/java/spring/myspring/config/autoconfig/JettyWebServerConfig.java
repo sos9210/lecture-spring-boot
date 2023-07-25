9c96d6c730862853557ed442f44e06342089428d
sungsanghyun <sos9210@naver.com>
2023-07-26 00:10:25 +0900

    조건부 자동 구성

--- helloboot/src/main/java/spring/myspring/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/spring/myspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,32 +1,34 @@
 package spring.myspring.config.autoconfig;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 import spring.myspring.config.ConditionalMyOnClass;
 import spring.myspring.config.MyAutoConfiguration;
 
-//@Configuration
 @MyAutoConfiguration
-//@Conditional(TomcatWebServerConfig.TomcatCondition.class)
-@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
-    //Conditional로 체크하는 시점에는 이미 우리가 작성한 빈은 스프링컨테이너에 등록된상태이다.
-    //이미 등록된 빈이 있으면 이 ServletWebServerFactory빈을 등록하지 않는다.
+//자동구성을 사용할지 여부를 정하는 Condition인터페이스를 구현한 클래스를 지정한다.
+//@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
-//    static class TomcatCondition implements Condition {
+//    static class JettyCondition implements Condition {
 //        @Override
 //        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
 //            //인자로 주어진 클래스가 포함되어 존재하는지 여부
-//            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",context.getClassLoader());
+//            return ClassUtils.isPresent("org.eclipse.jetty.server.Server",context.getClassLoader());
 //        }
 //    }
 }
---
+++ helloboot/src/test/java/spring/study/ConditionalTest.java
@@ -0,0 +1,110 @@
+package spring.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
+public class ConditionalTest {
+
+    @Test
+    void conditional() {
+        //true
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config1.class)
+                .run(context -> {
+                    Assertions.assertThat(context).hasSingleBean(MyBean.class);
+                    Assertions.assertThat(context).hasSingleBean(Config1.class);
+                });
+
+//        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+//        ac.register(Config1.class);
+//        ac.refresh();
+//
+//        MyBean bean = ac.getBean(MyBean.class);
+
+
+        //false
+        ApplicationContextRunner contextRunner2 = new ApplicationContextRunner();
+        contextRunner2.withUserConfiguration(Config2.class)
+                .run(context -> {
+                    Assertions.assertThat(context).doesNotHaveBean(MyBean.class);
+                    Assertions.assertThat(context).doesNotHaveBean(Config2.class);
+                });
+//        AnnotationConfigApplicationContext ac2 = new AnnotationConfigApplicationContext();
+//        ac2.register(Config2.class);
+//        ac2.refresh();
+//
+//        MyBean bean2 = ac2.getBean(MyBean.class);
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(BooleanCondition.class)
+    @interface  BooleanConditional{
+        boolean value();
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(TrueCondition.class)
+    @interface  TrueConditional{}
+
+    @Configuration
+//    @Conditional(TrueCondition.class)
+//    @TrueConditional
+    @BooleanConditional(true)
+    static class Config1 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(FalseCondition.class)
+    @interface  FalseConditional{}
+
+    @Configuration
+//    @Conditional(FalseCondition.class)
+//    @FalseConditional
+    @BooleanConditional(false)
+    static class Config2 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+    static class MyBean {}
+
+    static class TrueCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
+    }
+    static class FalseCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
+
+    static class BooleanCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            //애노테이션 메타정보를 가져와서 true인지 false인지 정하는 로직을 작성하는식으로 활용한다.
+            Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(BooleanConditional.class.getName());
+            Boolean value = (Boolean) annotationAttributes.get("value");
+            return value;
+        }
+    }
+}
